1c43f10e6234994147d624ee2dab6c4f438e5b68
aa800033 <aa800033@MI121-01.iem>
2023-10-17 17:04:01 +0200

    les differérents fruits et le panier

---
+++ mavenproject1/src/main/java/fr/ufrsciencestech/mavenproject1/App.java
@@ -0,0 +1,13 @@
+package fr.ufrsciencestech.mavenproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

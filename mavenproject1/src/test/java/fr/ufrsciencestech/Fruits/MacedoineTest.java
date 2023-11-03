300dbdafa57045a079a25b4a3d0e70e83b1eeac2
aa800033 <aa800033@MI121-01.iem>
2023-11-03 21:11:36 +0100

    Modification de MacedoineTest

--- mavenproject1/src/test/java/fr/ufrsciencestech/Fruits/MacedoineTest.java
+++ mavenproject1/src/test/java/fr/ufrsciencestech/Fruits/MacedoineTest.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -20,6 +21,10 @@ public class MacedoineTest {
     private Macedoine macedoine ;
     private ArrayList<Fruit> listeFruit;//une liste de fruits
     
+    //test d'intégration
+    private Ananas ananas;
+    private Banane banane;
+    
     @Before
     public void setUp(){
         fruit1 = mock(Orange.class); //doublures
@@ -28,6 +33,10 @@ public class MacedoineTest {
         listeFruit = new ArrayList<Fruit>();
         listeFruit.add(fruit1);
         listeFruit.add(fruit2);
+        
+        
+        ananas = new Ananas();
+        banane = new Banane(1.5 ,"Inde");        
     }
     
     @After
@@ -35,7 +44,7 @@ public class MacedoineTest {
         
     }
      @Test
-     public void testGetPrix(){
+     public void testGetPrixMock(){
         when(fruit1.getPrix()).thenReturn(1.5); //comportements des doublures
         when(fruit2.getPrix()).thenReturn(2.0);
         macedoine.AjoutFruitMacedoine(fruit1);
@@ -48,10 +57,10 @@ public class MacedoineTest {
      }
      
     /**
-     *Permet vérifier si un fruit est belle et bien ajouter à une macedoine
+     *Permet vérifier si un fruit est belle et bien ajouter à une macedoine en utilisant Mock
      */
     @Test 
-     public void testAjoutFruitMacedoine(){//Macedoine vide 
+     public void testAjoutFruitMacedoineMock(){//Macedoine vide 
         macedoine.AjoutFruitMacedoine(fruit1);
         macedoine.AjoutFruitMacedoine(fruit2);
          assertTrue(macedoine.getNbreElement()==2);
@@ -61,7 +70,7 @@ public class MacedoineTest {
      *Test si on peut ajouter à une macedoine directement une liste des fruits (pas à l'unité)
      */
     @Test
-     public void testAjoutListeFruitMacedoine(){
+     public void testAjoutListeFruitMacedoineMock(){
          
          macedoine.AjoutListeFruitMacedoine(listeFruit);
          assertTrue(macedoine.getNbreElement()==2);
@@ -76,14 +85,55 @@ public class MacedoineTest {
     /**
      *Tester pour voir si on a le même affichage de l'origine de la macedoine
      */
-    @Test
+    /*@Test
      public void testGetOrigine(){
          macedoine.AjoutFruitMacedoine(fruit1);
          when(fruit1.getOrigine()).thenReturn("France");
          String resultat = macedoine.getOrigine();
          System.out.println(resultat);
          assertEquals("Macedoine  de fruits d'origine :France", resultat);
+     }*/
+     
+     //les différents methodes de tests d'intégration,ils terminent par Int pour dire intégration
+     @Test
+     public void testGetPrixInt(){
+       
+        macedoine.AjoutFruitMacedoine(ananas);
+        macedoine.AjoutFruitMacedoine(banane);
+        double res = macedoine.getPrix(); 
+        assertTrue(res ==4.5); //le prix de l'ananas par défaut est à 3€
      }
      
-    
+     @Test 
+     public void testAjoutFruitMacedoineInt(){
+        //Macedoine vide 
+        assertFalse(macedoine.getNbreElement()==2);//pour vérifier si vraiment la macedoine est vide au départ
+        macedoine.AjoutFruitMacedoine(ananas);
+        macedoine.AjoutFruitMacedoine(banane);
+         assertTrue(macedoine.getNbreElement()==2);
+     }
+     
+     @Test
+     public void testAjoutListeFruitMacedoineInt(){
+         ArrayList<Fruit> liste = new ArrayList<Fruit>();
+         liste.add(ananas);liste.add(banane);
+         assertTrue(macedoine.getNbreElement()==0);
+         macedoine.AjoutFruitMacedoine(new Orange());
+         assertTrue(macedoine.getNbreElement()==1);
+         macedoine.AjoutListeFruitMacedoine(listeFruit);
+         assertTrue(macedoine.getNbreElement()==3);
+         assertFalse(macedoine.getNbreElement()!=3);
+     }
+     /*@Test
+     public void testGetOrigine(){
+         macedoine.AjoutFruitMacedoine(ananas);
+         String resultat = macedoine.getOrigine();
+         System.out.println(resultat);
+         String output = "Macedoine  de fruits d'origine :\n";
+         output+=ananas.toString()+"\n";
+                  System.out.println(output);
+
+         assertTrue(output.equalsIgnoreCase(resultat) );
+     }
+    */
 }
